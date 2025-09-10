2cb3f40fe31cceb5eedcbbb03c3690abf3ed3755
patri <patrik.golian888@gmail.com>
2025-09-10 16:13:03 +0200

    finished interfaces and repositories per each Entity

--- Server/Entities/Post.cs
+++ Server/Entities/Post.cs
@@ -2,7 +2,8 @@
 
 public class Post
 {
-    public required string Title { get; set; }
-    public required string Body { get; set; }
-    public required int Id { get; set; }
+    public  string Title { get; set; }
+    public  string Body { get; set; }
+    public  int Id { get; set; }
+    public  int UserId { get; set; }
 }
--- Server/Entities/User.cs
+++ Server/Entities/User.cs
@@ -2,7 +2,7 @@
 
 public class User
 {
-    public required string Username { get; set; }
-    public required string Password { get; set; }
-    public required int Id { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public int Id { get; set; }
 }
